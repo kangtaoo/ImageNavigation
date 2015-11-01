02cc1e93876a9bd973e64500cddc022fadc6c2ac
Tao Kang <kangkang@Taos-MBP.T-mobile.com>
2015-11-01 15:46:38 -0500

    Add inner class preview for camera activity

--- app/src/main/java/com/example/imagingnavigator/activities/CameraBasedViewActivity.java
+++ app/src/main/java/com/example/imagingnavigator/activities/CameraBasedViewActivity.java
@@ -1,7 +1,11 @@
 package com.example.imagingnavigator.activities;
 
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 import com.example.imagingnavigator.imagingnavigator.R;
 
@@ -13,6 +17,9 @@ import com.example.imagingnavigator.imagingnavigator.R;
 public class CameraBasedViewActivity extends Activity {
 
     private static final String TAG = CameraBasedViewActivity.class.getSimpleName();
+
+    private Camera camera;
+//    private
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,54 @@ public class CameraBasedViewActivity extends Activity {
     private void backMapBasedView() {
         setResult(RESULT_CANCELED);
         finish();
+    }
+
+    private boolean safeCameraOpen(int camId){
+        boolean camOpened = false;
+        try{
+            releaseCameraAndPreview();
+
+        }catch (Exception e){
+
+        }
+
+        return camOpened;
+    }
+
+
+    private void releaseCameraAndPreview(){
+
+    }
+
+    public class Preview implements SurfaceHolder.Callback{
+        SurfaceView surView;
+        SurfaceHolder surHolder;
+
+        Preview(Context context){
+//        super(context);
+
+            surView = new SurfaceView(context);
+
+            surHolder = surView.getHolder();
+            surHolder.addCallback(this);
+            surHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        }
+
+        @Override
+        public void surfaceCreated(SurfaceHolder holder){
+
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int w, int h){
+            // do nothing
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder){
+
+        }
+
     }
 
 }
