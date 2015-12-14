e83470ae49096b361e4db948b6fe40957062f44f
Tao Kang <kangkang@172-16-12-240.DYNAPOOL.NYU.EDU>
2015-12-13 20:39:13 -0500

    Calculate next point/step by using the first point/step in the list, once pass current next step, remove corresponding segment/steps from list. Works well on genymotion, but still get problem on real test. Maybe caused by sensor accuracy

--- app/src/main/java/com/example/imagingnavigator/function/Navigator.java
+++ app/src/main/java/com/example/imagingnavigator/function/Navigator.java
@@ -14,62 +14,69 @@ import java.util.*;
  */
 public class Navigator {
     private static final String TAG = Navigator.class.getSimpleName();
+    private static final int ERROR_ALLOWED = 10;
 
     //Compute the dot product AB . AC
     // A->B stands for the segment, C stands for current location
-    static private double dotProduct(double[] pointA, double[] pointB, double[] pointC)
-    {
-        double[] AB = new double[2];
-        double[] BC = new double[2];
-        AB[0] = pointB[0] - pointA[0];
-        AB[1] = pointB[1] - pointA[1];
-        BC[0] = pointC[0] - pointB[0];
-        BC[1] = pointC[1] - pointB[1];
-        double dot = AB[0] * BC[0] + AB[1] * BC[1];
-
-        return dot;
-    }
-
-    //Compute the cross product AB x AC
-    // A->B stands for the segment, C stands for current location
-    static private double CrossProduct(double[] pointA, double[] pointB, double[] pointC)
-    {
-        double[] AB = new double[2];
-        double[] AC = new double[2];
-        AB[0] = pointB[0] - pointA[0];
-        AB[1] = pointB[1] - pointA[1];
-        AC[0] = pointC[0] - pointA[0];
-        AC[1] = pointC[1] - pointA[1];
-        double cross = AB[0] * AC[1] - AB[1] * AC[0];
-
-        return cross;
-    }
+//    static private double dotProduct(double[] pointA, double[] pointB, double[] pointC)
+//    {
+//        double[] AB = new double[2];
+//        double[] BC = new double[2];
+//        AB[0] = pointB[0] - pointA[0];
+//        AB[1] = pointB[1] - pointA[1];
+//        BC[0] = pointC[0] - pointB[0];
+//        BC[1] = pointC[1] - pointB[1];
+//        double dot = AB[0] * BC[0] + AB[1] * BC[1];
+//
+//        return dot;
+//    }
+//
+//    //Compute the cross product AB x AC
+//    // A->B stands for the segment, C stands for current location
+//    static private double CrossProduct(double[] pointA, double[] pointB, double[] pointC)
+//    {
+//        double[] AB = new double[2];
+//        double[] AC = new double[2];
+//        AB[0] = pointB[0] - pointA[0];
+//        AB[1] = pointB[1] - pointA[1];
+//        AC[0] = pointC[0] - pointA[0];
+//        AC[1] = pointC[1] - pointA[1];
+//        double cross = AB[0] * AC[1] - AB[1] * AC[0];
+//
+//        return cross;
+//    }
 
     //Compute the distance from A to B
     static double distance(double[] pointA, double[] pointB)
     {
-        double d1 = pointA[0] - pointB[0];
-        double d2 = pointA[1] - pointB[1];
+        Location locA = new Location("");
+        Location locB = new Location("");
 
-        return Math.sqrt(d1 * d1 + d2 * d2);
+        locA.setLatitude(pointA[0]);
+        locA.setLongitude(pointA[1]);
+
+        locB.setLatitude(pointB[0]);
+        locB.setLongitude(pointB[1]);
+        
+        return locA.distanceTo(locB);
     }
 
     //Compute the distance from AB to C
     // A->B stands for the segment, C stands for current location
-    static private double pointToSegmentDistance(double[] pointA, double[] pointB, double[] pointC)
-    {
-        double dist = CrossProduct(pointA, pointB, pointC) / distance(pointA, pointB);
-
-        double dot1 = dotProduct(pointA, pointB, pointC);
-        if (dot1 > 0)
-            return distance(pointB, pointC);
-
-        double dot2 = dotProduct(pointB, pointA, pointC);
-        if (dot2 > 0)
-            return distance(pointA, pointC);
-
-        return Math.abs(dist);
-    }
+//    static private double pointToSegmentDistance(double[] pointA, double[] pointB, double[] pointC)
+//    {
+//        double dist = CrossProduct(pointA, pointB, pointC) / distance(pointA, pointB);
+//
+//        double dot1 = dotProduct(pointA, pointB, pointC);
+//        if (dot1 > 0)
+//            return distance(pointB, pointC);
+//
+//        double dot2 = dotProduct(pointB, pointA, pointC);
+//        if (dot2 > 0)
+//            return distance(pointA, pointC);
+//
+//        return Math.abs(dist);
+//    }
 
     /*
      * 1. First we find the nearest segment among all steps in the path, we take this is the current
@@ -78,38 +85,52 @@ public class Navigator {
      */
     static public double[] getTargetPoint(List<double[]> path,
                                    double[] curPosition){
-        double min = Double.MAX_VALUE;
-        double dist;
-        double[] target = path.get(0);
-        for(int i = 1; i < path.size(); i++){
-            // For each segement [path[i-1], path[i]], calculate distance from current position.
-            // Keep recording the min distance, as well as the ending point of that segment.
-            dist = pointToSegmentDistance(path.get(i-1), path.get(i), curPosition);
-            if(dist < min){
-                min = dist;
-                target = path.get(i);
+//        double min = Double.MAX_VALUE;
+//        double dist;
+//        double[] target = path.get(0);
+//        for(int i = 1; i < path.size(); i++){
+//            // For each segement [path[i-1], path[i]], calculate distance from current position.
+//            // Keep recording the min distance, as well as the ending point of that segment.
+//            dist = pointToSegmentDistance(path.get(i-1), path.get(i), curPosition);
+//            if(dist < min){
+//                min = dist;
+//                target = path.get(i);
+//            }
+//        }
+//        return target;
+
+        if(arrived(curPosition, path.get(0))){
+            if(path.size() > 1){
+                path.remove(0);
+
             }
         }
-
-        return target;
+        return path.get(0);
     }
 
     static public Step getCurrentStep(List<Step> steps, double[] curPosition){
-        double min = Double.MAX_VALUE;
-        double dist;
-        Step result = steps.get(0);
-        Step cur;
+//        double min = Double.MAX_VALUE;
+//        double dist;
+//        Step result = steps.get(0);
+//        Step cur;
+//
+//        for(int i = 0; i < steps.size(); i++){
+//            cur = steps.get(i);
+//            dist = pointToSegmentDistance(cur.getStart(), cur.getEnd(), curPosition);
+//            if(dist < min){
+//                min = dist;
+//                result = cur;
+//            }
+//        }
+//
+//        return result;
 
-        for(int i = 0; i < steps.size(); i++){
-            cur = steps.get(i);
-            dist = pointToSegmentDistance(cur.getStart(), cur.getEnd(), curPosition);
-            if(dist < min){
-                min = dist;
-                result = cur;
+        if(arrived(curPosition, steps.get(0).getEnd())){
+            if(steps.size() > 1){
+                steps.remove(0);
             }
         }
-
-        return result;
+        return steps.get(0);
     }
 
 
@@ -168,5 +189,9 @@ public class Navigator {
             eta += steps.get(i).getDuration();
         }
         return eta;
+    }
+
+    static public boolean arrived(double[] cur, double[] dest){
+        return distance(cur, dest) <= ERROR_ALLOWED;
     }
 }
